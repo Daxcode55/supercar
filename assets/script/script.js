22f7babba6d0b278a7779ee602bf8e8d60d4ba2b
Daxcode55 <daxcode198@gmail.com>
2023-12-05 11:44:32 -0800

    content dashboard view

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,7 +4,9 @@ iconBurger.addEventListener("click", () => {
   console.log("tmbol di tekan");
   let asidenav = document.querySelector("#asideNav");
   let topnav = document.querySelector(".col-right");
+  let cardContent = document.querySelector(".container-card-content");
   asidenav.classList.toggle("slideactive");
   topnav.classList.toggle("navtopActive")
+  cardContent.classList.toggle("flexActive");
 });
 
